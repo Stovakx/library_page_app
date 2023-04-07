60f48873b3b2e87a939e39bcccc2c65c703cf180
Stovakx <112606493+Stovakx@users.noreply.github.com>
2023-04-08 00:30:34 +0200

    books update
    add some styling with bootstrap, card, image path and show. In readme is next steps,

--- routes/books.js
+++ routes/books.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const multer = require('multer')
-const path = require('path')
 const fs = require('fs')
 const Book = require('../models/book')
 const Author = require('../models/author')
+const multer = require('multer')
+const path = require('path')
 const uploadPath = path.join('public', Book.coverImageBasePath)
 const imageMimeTypes = ['image/jpeg', 'image/png', 'images/gif']
 const upload = multer({
@@ -20,12 +20,13 @@ router.get('/', async (req, res) => {
   if (req.query.title != null && req.query.title != '') {
     query = query.regex('title', new RegExp(req.query.title, 'i'))
   }
-  if (req.query.publishedBefore != null && req.query.publishedBefore != '') {
+  //can use for filters
+/*   if (req.query.publishedBefore != null && req.query.publishedBefore != '') {
     query = query.lte('publishDate', req.query.publishedBefore)
   }
   if (req.query.publishedAfter != null && req.query.publishedAfter != '') {
     query = query.gte('publishDate', req.query.publishedAfter)
-  }
+  } */
   try {
     const books = await query.exec()
     res.render('books/index', {
