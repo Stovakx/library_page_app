50ad0aa982023378e0fa443a5785ff2fafc26323
Stovakx <112606493+Stovakx@users.noreply.github.com>
2023-04-18 13:32:29 +0200

    user model

---
+++ models/user.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+
+const userSchema = new mongoose.Schema ({
+    user:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password:{
+        type:String,
+        required:true,
+        
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    favorieGenre:{
+        type: String,
+        required: false,
+    },
+    gender:{
+        type:String,
+        required: false,
+    },
+    age:{
+        type: Number,
+        required: false
+    }
+})
+
+userSchema.pre('save', async function(next) {
+    const user = this
+  
+    if (!user.isModified('password')) {
+      return next()
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(user.password, salt)
+    
+    user.password = hash
+    next()
+})
+
+userSchema.methods.comparePassword = async function(candidatePassword){
+    const user = this
+    return bcrypt.compare(candidatePassword, user.password)
+}
+
+module.exports = mongoose.module('User', userSchema)
+
