7410e785b5c623365fefbe2a60a2ae539a48f549
Stovakx <112606493+Stovakx@users.noreply.github.com>
2023-04-26 20:12:11 +0200

    completet log/reg forms
    registration works, everytime i wrote password, it shows invalid. Need to closer look on it.

--- routes/user.js
+++ routes/user.js
@@ -1,47 +1,76 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
+const bcrypt = require('bcrypt')
 
-router.get('/register', (req, res) =>{
-  res.render('user/register')
-})
-
-router.post('/register', async (req, res) => {
-  const { email, password } = req.body;
-
-  try {
-    const user = new User({ user, email, password });
-    await user.save();
-    res.status(201).send(user);
-  } catch (err) {
-    res.status(400).send(err.message);
-  }
-});
-
+//show login page
 router.get('/login', (req, res) => {
   res.render('user/login')
 })
 
 router.post('/login', async (req, res) => {
-    const { user, password } = req.body;
-  
-    try {
-      const userName = await User.findOne({ user });
-  
-      if (!userName) {
-        return res.status(401).send('Invalid user name');
-      }
-  
-      const isMatch = await user.comparePassword(password);
-  
-      if (!isMatch) {
-        return res.status(401).send('Invalid password');
-      }
-  
-      res.send(user);
-    } catch (err) {
-      res.status(400).send(err.message);
+  const { user, password } = req.body;
+
+  try {
+    const foundUser = await User.findOne({ user });
+
+    if (!foundUser) {
+      return res.status(401).send('Invalid user name');
     }
-  });
+
+    const isMatch = await foundUser.comparePassword(password);
+
+    if (!isMatch) {
+      return res.status(401).send('Invalid password');
+    }
+
+    res.send(foundUser);
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+});
+
   
+
+//register page
+router.get('/register', (req, res) =>{
+  res.render('user/register')
+})
+
+//creating new user
+router.post('/register', async (req, res) => {
+
+  try {
+    
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    const user = new User({
+      user: req.body.user,
+      email: req.body.email,
+      password: hashedPassword,
+      favoriteGenre: req.body.genre,
+      gender: req.body.gender,
+      age: req.body.age
+    })
+    const newUser = await user.save()
+    res.redirect(`/${newUser.id}`)
+  } catch (err) {
+    res.status(400).send(err.message);
+    res.redirect('/register')
+  }
+});
+
+//show user page 
+router.get('/:id', async (req, res) => {
+  try{
+    const user = await User.findById(req.params.id)
+    res.render('user/user', {
+      user: user,
+    })
+  }catch {
+    res.redirect('/')
+  }
+
+})
+
+
 module.exports = router;
